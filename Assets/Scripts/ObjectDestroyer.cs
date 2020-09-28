4236a2c77b644a6c1fe9838ae2effcb3f1dfab18
Debbie Meda <54830275+bitmonst3r@users.noreply.github.com>
2020-09-27 17:58:35 -0700

    Platformer (Part 1)
    Project 2.
    Platformer (Part 1)
    Super Mario Bros.

---
+++ Assets/Scripts/ObjectDestroyer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class ObjectDestroyer : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        // 1. Test for mouse click
+        if (Input.GetMouseButtonUp(0))
+        {
+            // 2. Get mouse position in world space
+            Vector3 worldMousePosition = Camera.main.ScreenToWorldPoint(
+                new Vector3(Input.mousePosition.x, Input.mousePosition.y, 100f));
+
+            // 3. Get direction vector from camera position to mouse position in world space
+            Vector3 direction = worldMousePosition - Camera.main.transform.position;
+
+            RaycastHit hit;
+
+            // 4. Cast a ray from the camera to the direction
+            if (Physics.Raycast(Camera.main.transform.position, direction, out hit, 100f))
+            {
+                Debug.DrawLine(Camera.main.transform.position, hit.point, Color.green, 0.5f);
+                Destroy(hit.collider.gameObject);
+            } else
+            {
+                Debug.DrawLine(Camera.main.transform.position, worldMousePosition, Color.red, 0.5f);
+            }
+            
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    float timeLeft = 100.0f;
+    public Text text;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // time goes backwards, Time.deltaTime
+        timeLeft -= Time.deltaTime;
+        text.text = "" + Mathf.Round(timeLeft);
+        if(timeLeft < 0)
+        {
+            Debug.Log("GAME OVER");
+        }
+    }
+}
